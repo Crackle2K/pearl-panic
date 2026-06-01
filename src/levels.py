40787940c47b36e669fcbcf3490611a2f3017f5a
dhani-shah20 <dhanishah2008@gmail.com>
2026-06-01 13:24:07 -0400

    Update levels.py

--- src/levels.py
+++ src/levels.py
@@ -1,27 +1,31 @@
-import data, pygame 
+import data, pygame, os
 
 class levels():
     def __init__(self):
         pygame.init()
-        clock = pygame.time.Clock()
+        self.clock = pygame.time.Clock()
         self.screen = pygame.display.set_mode((680,480))
         self.screen.blit()
         pygame.display.flip()
 class level1(levels):
     def __init__(self):
         super().__init__()
-        bg_img = pygame.image.load('beach.png').convert() 
-        bg_img = pygame.transform.scale(bg_img, (680, 480))
+        beach_path = os.path.join(os.path.dirname(__file__), "assets", "images", "beach.png")
+        beach_img = pygame.image.load(beach_path).convert()
+        beach_img = pygame.transform.scale(beach_img, (self.SCREEN_WIDTH, self.SCREEN_HEIGHT))
 
 class level2(levels):
     def __init__(self):
         super().__init__()
-        bg_img = pygame.image.load('ocean.png').convert()
-        bg_img = pygame.transform.scale(bg_img, (680, 480))
+        ocean_path = os.path.join(os.path.dirname(__file__), "assets", "images", "beach.png")
+        ocean_img = pygame.image.load(ocean_path).convert()
+        ocean_img = pygame.transform.scale(ocean_img, (self.SCREEN_WIDTH, self.SCREEN_HEIGHT))
 class level3(levels):
     def __init__(self):
         super().__init__() 
-        bg_img = pygame.image.load('cave.png').convert()
-        bg_img = pygame.transform.scale(bg_img, (680, 480))
+        cave_path = os.path.join(os.path.dirname(__file__), "assets", "images", "beach.png")
+        cave_img = pygame.image.load(cave_path).convert()
+        cave_img = pygame.transform.scale(cave_img, (self.SCREEN_WIDTH, self.SCREEN_HEIGHT))
+
 
 
